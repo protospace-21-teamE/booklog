fdc29d895112a0d6f170c0068e600704fb909ccc
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-28 13:59:24 +0900

    Set routing(users)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "books#index"
+  root "books#index"
+
+  resources :users, only: [:show, :edit, :update]
 end
