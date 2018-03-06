42430fa2de88d45cabe56f5d163a0064fe20e9b2
aaing <n.y1031@gmail.com>
2018-03-06 12:15:16 +0900

    bundle exec rake db:migarate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228094125) do
+ActiveRecord::Schema.define(version: 20180306030646) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                      null: false
@@ -35,7 +35,17 @@ ActiveRecord::Schema.define(version: 20180228094125) do
     t.index ["name"], name: "index_books_on_name", using: :btree
   end
 
-  add_foreign_key "books", "authors"
+  create_table "shelves", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "name",        limit: 65535
+    t.text     "description", limit: 65535
+    t.integer  "mode"
+    t.integer  "design"
+    t.integer  "is_public"
+    t.integer  "user_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["user_id"], name: "index_shelves_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                                default: "", null: false
@@ -67,4 +77,6 @@ ActiveRecord::Schema.define(version: 20180228094125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "books", "authors"
+  add_foreign_key "shelves", "users"
 end
