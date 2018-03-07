aba8402d8bd037e126e03beac044ca5ecce0dc0b
aaing <n.y1031@gmail.com>
2018-03-07 13:11:11 +0900

    adit reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+  def create
+    review = Review.new(create_params)
+    review.save
+    book = review.book
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def create_params
+    params.permit(:book_id).merge(user_id: current_user.id)
+  end
 end
