0e53db72f57dc66863b6dae4b6a5de1d0a1dd8a0
aaing <n.y1031@gmail.com>
2018-03-10 17:16:02 +0900

    rewrite conditional statement using ternary operator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,11 @@
 module ApplicationHelper
 
   def image_url(book)
-    if book.image.present?
-      url = book.image
-    else
-      url = '/assets/noimage_m.png'
-    end
+    book.image.present? ? url = book.image : url = '/assets/noimage_m.png'
   end
 
   def released_date(book)
-    if book.released_on.present?
-      released_date = '(' + book.decorate_released_on + '発売)'
-    else
-      released_date = ''
-    end
+    book.released_on.present? ? released_date = '(' + book.decorate_released_on + '発売)' : released_date = ''
   end
 
 end
