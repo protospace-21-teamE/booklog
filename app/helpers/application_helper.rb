a16e03c809b9e3c2e870a6a0d9e4390035dc5c79
aaing <n.y1031@gmail.com>
2018-03-10 15:04:35 +0900

    add image_url method and released_date in application helper
    to avoid fat view and util view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def image_url(book)
+    if book.image.present?
+      url = book.image
+    else
+      url = '/assets/noimage_m.png'
+    end
+  end
+
+  def released_date(book)
+    if book.released_on.present?
+      released_date = '(' + book.decorate_released_on + '発売)'
+    else
+      released_date = ''
+    end
+  end
+
 end
