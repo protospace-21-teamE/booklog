1dd4fe4f6d4876db84361b8156fd4043142dc80a
aaing <n.y1031@gmail.com>
2018-03-10 13:59:39 +0900

    edit books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def show
     book = Book.find(params[:id])
     @book = BookDecorator.new(book)
+    @review = book.reviews.where(user_id: current_user.id).first_or_initialize
   end
 
 end
