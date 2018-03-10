60b41d298d7eabde7cc0c5b67a88a0acaa008fb0
aaing <n.y1031@gmail.com>
2018-03-10 17:13:13 +0900

    fix indent

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,8 +1,8 @@
 class BookDecorator < Draper::Decorator
   delegate_all
 
-    def decorate_released_on
-      book.released_on.strftime("%Y年%m月%d日")
-    end
+  def decorate_released_on
+    book.released_on.strftime("%Y年%m月%d日")
+  end
 
 end
