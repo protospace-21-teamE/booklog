6ab532bcbf2ebd8dcca80e5c790e7b19e45ac0e0
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-03-10 15:46:48 +0900

    Set shelves controller

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,4 +1,32 @@
 class ShelvesController < ApplicationController
+  before_action :set_shelf
+  def edit
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    @shelf.update(shelf_params)
+    redirect_to edit_user_shelf_path
+  end
+
   def show
+    @user = User.find(params[:user_id])
+    @reviews = Review.where(user_id: params[:user_id]).order("id DESC").includes(:book)
+    render layout: "shelf_show"
+  end
+
+  private
+  def set_shelf
+    @shelf = Shelf.find(params[:id])
+  end
+
+  def shelf_params
+    params.require(:shelf).permit(
+      :name,
+      :description,
+      :mode,
+      :design,
+      :is_public
+    ).merge(user_id: params[:user_id])
   end
 end
