5b6ffaeb49f3f437cff7e9bd2328cba402b920f4
aaing <n.y1031@gmail.com>
2018-03-16 14:52:01 +0900

    implement action edit and update, and set strong parameter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,37 @@
 class ReviewsController < ApplicationController
+  before_action: :set_review, only: [:edit, :update]
+
   def create
-    review = Review.new(create_params)
+    review = Review.new(review_create_params)
     review.save
     book = review.book
     redirect_to book_path(book)
   end
 
+  def edit
+    @book = @review.book
+  end
+
+  def update
+    if @review.update(review_update_params)
+          redirect_to edit_book_review_path
+    else
+      flash.now[:alert] = "レビューの更新ができませんでした"
+      render :edit
+    end
+  end
+
   private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 
   def review_create_params
     params.require(:review).permit(:is_public, :reading_status).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
+
+  def review_update_params
+    params.require(:review).permit(:is_public, :reading_status, :rate, :body, :read_on, :comment)
   end
 end
