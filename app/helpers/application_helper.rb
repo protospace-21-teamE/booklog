830f7d9f35e591b55dadafd53624bc96a3296004
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-03-16 21:30:27 +0900

    Set media_type & released_on method at helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     book.released_on.present? ? released_date = '(' + book.decorate_released_on + '発売)' : released_date = ''
   end
 
+  def media_type(book)
+    book.media_type == 0 ? "本 /" : "電子書籍 /"
+  end
+
+  def released_on(book)
+    book.released_on.present? ? "#{book.released_on.strftime("%Y年%-m月%d日")} /" : ""
+  end
+
 end
