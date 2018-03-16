4414975b14360ee9b8e040231366033577f36eeb
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-03-16 18:33:18 +0900

    Modify search action at books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,9 @@ class BooksController < ApplicationController
   end
 
   def search
-    @books = Book.where('name LIKE(?)', "%#{params[:name]}%")
+    @books = Book.where('name LIKE(?)', "%#{params[:name]}%").includes(:author).page(params[:page]).per(10)
+    reviews = Review.where(user_id: current_user.id).includes(:book)
+    @book_ids = reviews.group(:book_id).count.keys
+    @review = Review.new
   end
-
 end
